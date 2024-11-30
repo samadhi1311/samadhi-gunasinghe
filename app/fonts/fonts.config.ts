8c1e3f158ce79af1527c49d5492f3ff3542ae4ce
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-30 21:12:04 +0530

    Added custom local fonts

---
+++ app/fonts/fonts.config.ts
@@ -0,0 +1,8 @@
+import { Geist } from 'next/font/google';
+
+export const geist = Geist({
+	weight: ['200', '300', '400', '500', '600', '700', '800'],
+	display: 'swap',
+	subsets: ['latin'],
+	variable: '--font-geist',
+});
