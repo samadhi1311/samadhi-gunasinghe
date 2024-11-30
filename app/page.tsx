5f5d7298b48160a3e9a06707e955e59205bafbdd
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-30 22:32:12 +0530

    Fixed text size issues

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
 
 export default function Home() {
 	return (
-		<main className='flex min-h-svh flex-col items-center justify-center max-w-screen-2xl'>
-			<h1 className='text-2xl lg:text-8xl font-dirtyline tracking-wide lowercase flex flex-col leading-none text-white/60'>
+		<main className='mx-auto flex h-svh max-w-screen-2xl flex-col items-center justify-center'>
+			<h1 className='font-dirtyline flex flex-col text-2xl lowercase tracking-wide text-white/60 lg:text-6xl'>
 				<span className='self-start font-thin'>Hello, I&apos;m </span>
-				<span className='text-4xl lg:text-[10rem] self-center text-white'>Samadhi Gunasinghe</span>
+				<span className='self-center text-4xl text-white lg:text-9xl'>Samadhi Gunasinghe</span>
 				<span className='self-end font-thin'>a creative web developer</span>
 				<span className='self-end font-thin'>and designer</span>
 			</h1>
