fddff8c77b69d7a7c757326fb7034b800b955aa6
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-11-30 23:21:56 +0530

    Added retro grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 'use client';
 
+import RetroGrid from '@/components/ui/retro-grid';
+import { motion } from 'motion/react';
+
 export default function Home() {
 	return (
 		<main className='mx-auto flex h-svh max-w-screen-2xl flex-col items-center justify-center'>
-			<h1 className='font-dirtyline flex flex-col text-2xl lowercase tracking-wide text-white/60 lg:text-6xl'>
-				<span className='self-start font-thin'>Hello, I&apos;m </span>
-				<span className='self-center text-4xl text-white lg:text-9xl'>Samadhi Gunasinghe</span>
-				<span className='self-end font-thin'>a creative web developer</span>
-				<span className='self-end font-thin'>and designer</span>
-			</h1>
+			<motion.h1
+				className='font-dirtyline flex flex-col text-2xl lowercase tracking-wide text-white/60 md:text-4xl xl:text-6xl'
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 3, delay: 0.5, ease: [0.645, 0.045, 0.355, 1] }}>
+				<span className='self-start'>Hello, I&apos;m </span>
+				<span className='self-center text-4xl text-white md:text-7xl xl:text-9xl'>Samadhi Gunasinghe</span>
+				<span className='self-end'>a creative web developer</span>
+				<span className='self-end'>and designer</span>
+			</motion.h1>
+			<RetroGrid />
 		</main>
 	);
 }
