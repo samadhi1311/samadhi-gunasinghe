f4dd498acf71e21b323001c6fdc265846ebe3494
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-01 14:19:07 +0530

    Removed unused files

--- app/fonts/fonts.config.ts
+++
@@ -1,8 +0,0 @@
-import { Geist } from 'next/font/google';
-
-export const geist = Geist({
-	weight: ['200', '300', '400', '500', '600', '700', '800'],
-	display: 'swap',
-	subsets: ['latin'],
-	variable: '--font-geist',
-});
